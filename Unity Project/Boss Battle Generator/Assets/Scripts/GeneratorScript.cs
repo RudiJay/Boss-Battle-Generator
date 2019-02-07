3fb0319591b427fc6bf84955c5199648da534cdf
RudiJay <rudiprentice@gmail.com>
2019-02-07 18:34:26 +0000

    Added random colour button function with weighting

--- Unity Project/Boss Battle Generator/Assets/Scripts/GeneratorScript.cs
+++ Unity Project/Boss Battle Generator/Assets/Scripts/GeneratorScript.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class GeneratorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GeneratorScript Instance;
+
+    private GameObject Boss;
+
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        Boss = GameObject.FindWithTag("Boss");
     }
 
     public void GenerateBoss()
     {
+        if (Boss)
+        {
+            SpriteRenderer sprite = Boss.GetComponentInChildren<SpriteRenderer>();
 
+            if (sprite)
+            {
+                sprite.enabled = true;
+
+                sprite.color = Random.ColorHSV();
+
+                if (Time.frameCount % 5 == 0)
+                {
+                    sprite.color = Color.red;
+                }
+            }
+        }
     }
 }

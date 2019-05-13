691b3d7c756a3fbc9251f4bd26c36596bf0ef034
Rudi <rudiprentice@gmail.com>
2019-05-13 19:01:03 +0100

    Implemented projectile speed

--- Unity Project/Boss Battle Generator/Assets/Scripts/ProjectileLogic.cs
+++ Unity Project/Boss Battle Generator/Assets/Scripts/ProjectileLogic.cs
@@ -20,7 +20,7 @@ public class ProjectileLogic : MonoBehaviour
     {
         if (rb != null)
         {
-            rb.AddForce(transform.up * 100);
+            rb.AddForce(transform.up * speed * 100);
         }
 
         Invoke("DisableProjectile", selfDestructTime);

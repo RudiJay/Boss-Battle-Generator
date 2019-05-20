a4b4aa8504db1a10903997fbcf06f550f4c63a98
Rudi <rudiprentice@gmail.com>
2019-05-20 18:12:12 +0100

    Damaged player on collision with boss

--- Unity Project/Boss Battle Generator/Assets/Scripts/PlayerController.cs
+++ Unity Project/Boss Battle Generator/Assets/Scripts/PlayerController.cs
@@ -107,4 +107,12 @@ public class PlayerController : MonoBehaviour
             Debug.Log("ERROR: Available Projectile Not Found");
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Boss")
+        {
+            DamagePlayer();
+        }
+    }
 }
